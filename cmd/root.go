f21a554b804acf7e2a2a56d6fdf264dc2cbe28b2
Brayden Ooi <brayden.ooi@hotmail.com>
2024-04-07 01:38:45 +0800

    scaffold project

---
+++ cmd/root.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+// Main applications for this project.
+// The directory name for each application should match the name of the executable you want to have (e.g., /cmd/myapp).
+
+// If you think the code can be imported and used in other projects, then it should live in the /pkg directory.
+// If the code is not reusable or if you don't want others to reuse it, put that code in the /internal directory.
+
+func Execute() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brayden-ooi/bookkeeper
+
+go 1.22.2
