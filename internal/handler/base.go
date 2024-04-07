42b1f555d20603c6735d1f9b75da51307fea77d9
Brayden Ooi <brayden.ooi@hotmail.com>
2024-04-07 18:13:10 +0800

    update view directory structure and add fallback 404 page

--- internal/handler/base.go
+++ internal/handler/base.go
@@ -1,16 +1,27 @@
 package handler
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/a-h/templ"
-	page "github.com/brayden-ooi/bookkeeper/internal/view/pages/index"
+	page "github.com/brayden-ooi/bookkeeper/internal/view/pages"
 )
 
-func Base() *templ.ComponentHandler {
-	return templ.Handler(page.Index())
+func Base(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		page.NotFound().Render(context.Background(), w)
+
+		return
+	}
+	page.Index().Render(context.Background(), w)
 }
 
 func Ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong!"))
+}
+
+func NotFound() *templ.ComponentHandler {
+	return templ.Handler(page.Index())
 }
