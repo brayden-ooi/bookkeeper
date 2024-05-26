faa33d7f9bbb1f48fc61ab491e4bb5970ba07401
Brayden Ooi <brayden.ooi@hotmail.com>
2024-05-27 01:47:50 +0800

    change account id to numeric text

--- sql/queries/accounts.sql
+++ sql/queries/accounts.sql
@@ -1,7 +1,10 @@
 -- name: CreateAccount :one
-INSERT INTO accounts (name, type, owner_id) 
-VALUES (?, ?, ?)
+INSERT INTO accounts (id, name, type, owner_id) 
+VALUES (?, ?, ?, ?)
 RETURNING *;
 
 -- name: ListAccountsByUser :many
 SELECT * FROM accounts WHERE owner_id = ?;
+
+-- name: GetAccountByUserAndID :one
+SELECT * FROM accounts WHERE owner_id = ? AND id = ?;
