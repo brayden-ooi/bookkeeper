ed5a7966b4288501ca1dc56e5e35b74cda618ab1
Brayden Ooi <brayden.ooi@hotmail.com>
2024-06-23 17:50:46 +0800

    update user service

--- internal/service/user/main.go
+++ internal/service/user/main.go
@@ -5,18 +5,33 @@ import (
 
 	"github.com/brayden-ooi/bookkeeper/internal/database"
 	"github.com/brayden-ooi/bookkeeper/internal/service"
+	"github.com/brayden-ooi/bookkeeper/internal/utils"
 )
 
 type user_service struct {
-	ctx context.Context
+	user_id int64
+	ctx     context.Context
 }
 
 func Init(ctx context.Context) *user_service {
 	return &user_service{
-		ctx,
+		user_id: utils.MyContext{ctx}.GetUserID(),
+		ctx:     ctx,
 	}
 }
 
-func (srv *user_service) Create(name string) (database.User, error) {
-	return service.DB.CreateUser(srv.ctx, name)
+func Create(ctx context.Context, name string) (database.User, error) {
+	return service.DB.CreateUser(ctx, name)
+}
+
+// func GetByID(id int) database.User {
+// 	ctx := context.Background()
+
+// 	user, err := service.DB.GetUser(ctx, int64(id))
+
+// 	return user
+// }
+
+func (srv *user_service) GetTxCounter() (int64, error) {
+	return service.DB.GetTransactionCounter(srv.ctx, srv.user_id)
 }
